0f906543856f1a1a45b816a961ba4b8bcca5c722
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-21 11:21:05 -0700

    Add entity_id, entity_type_id, and type_id columns

--- sql/leogalle_test/comment/create.sql
+++ sql/leogalle_test/comment/create.sql
@@ -1,5 +1,8 @@
 CREATE TABLE `comment` (
     `comment_id` int(10) unsigned not null auto_increment,
+    `entity_id` int(10) unsigned default null,
+    `entity_type_id` int(10) unsigned not null,
+    `type_id` int(10) unsigned default null,
     `message` text not null,
     `created` datetime not null,
     PRIMARY KEY (`comment_id`)
