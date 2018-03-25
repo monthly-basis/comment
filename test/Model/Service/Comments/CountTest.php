b7ad5203a0bd10a63bcdcb2e33560cf6b285edc8
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-24 21:10:34 -0700

    Create preliminary count test

---
+++ test/Model/Service/Comments/CountTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace LeoGalleguillos\CommentTest\Model\Entity;
+
+use LeoGalleguillos\Comment\Model\Service as CommentService;
+use LeoGalleguillos\Comment\Model\Table as CommentTable;
+use PHPUnit\Framework\TestCase;
+
+class CountTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->commentTableMock = $this->createMock(
+            CommentTable\Comment::class
+        );
+        $this->countCommentsService = new CommentService\Comments\Count(
+            $this->commentTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            CommentService\Comments\Count::class,
+            $this->countCommentsService
+        );
+    }
+}
