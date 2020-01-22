5c868cdedb940c9db9759a40e4aa2192a65d5726
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-01-22 23:12:45 +0000

    Code cleanup

--- src/Model/Service/Comments.php
+++ src/Model/Service/Comments.php
@@ -9,32 +9,21 @@ use LeoGalleguillos\Entity\Model\Entity as EntityEntity;
 
 class Comments
 {
-    /**
-     * Construct.
-     *
-     * @param CommentFactory\Comment $commentFactory
-     * @param CommentTable\Comment $commentTable
-     */
     public function __construct(
         CommentFactory\Comment $commentFactory,
         CommentTable\Comment $commentTable
     ) {
-        $this->commentFactory        = $commentFactory;
-        $this->commentTable          = $commentTable;
+        $this->commentFactory = $commentFactory;
+        $this->commentTable   = $commentTable;
     }
 
     /**
-     * Get.
-     *
-     * @param EntityEntity\EntityType $entityTypeEntity
-     * @param int $typeId
      * @yield CommentEntity\Comment
-     * @return Generator
      */
     public function get(
         EntityEntity\EntityType $entityTypeEntity,
         int $typeId
-    ) : Generator {
+    ): Generator {
         $arrays = $this->commentTable->selectWhereEntityTypeIdAndTypeId(
             $entityTypeEntity->getEntityTypeId(),
             $typeId
